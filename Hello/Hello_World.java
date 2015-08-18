d07ec7af4219ee8d868dfe291029e83548624f4b
Kevinlinexo <khuntorialog30>
2015-08-18 09:20:22 -0700

    Hello
    Feeling great

---
+++ Hello/Hello_World.java
@@ -0,0 +1,14 @@
+
+/**
+ * This is the first project.
+ * 
+ * @author Kevin Lin 
+ * @version August 18,2015
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Kevin");
+    }    
+}
